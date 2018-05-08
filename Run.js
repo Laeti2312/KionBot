bf81d413bfe2fb4535903efc87269dea05845d4f
Laeti2312 <38733102+Laeti2312@users.noreply.github.com>
2018-05-08 11:50:13 +0200

    Update Run.js

--- Run.js
+++ Run.js
@@ -9,7 +9,7 @@ bot.on('message', async function (message){
         case "pong" : pong(message);
             break;
 
-        case "f!fuli" : await sleep(500 + random(500,0)); 
+        case "k!fuli" : await sleep(500 + random(500,0)); 
                       message.channel.send('Ugh, baboons');
                       await sleep(1000 + random(500,0));
                       message.channel.send('Seriously?');
@@ -19,18 +19,18 @@ bot.on('message', async function (message){
                       message.channel.send('Huwezi', {file: "https://cdn.discordapp.com/attachments/355274540248596480/356537450190995458/Fuli-eyes.gif"});
             break;
         
-        case "f!foolishm" : message.channel.send('MacGuy has 100% foolishness');
+        case "k!foolishm" : message.channel.send('MacGuy has 100% foolishness');
             break;
         
-        case "f!help" : message.channel.send('1) f!how are you = respond : How Foolish smth is');
-                        message.channel.send('2) f!foolish = respond : rate the foolishness of smth');
-                        message.channel.send('3) f!fuli = respond with waiting time : Ugh, baboons; Seriously?; Nowhere to run that I cannot run faster; Huwezi');
+        case "k!help" : message.channel.send('1) k!how are you = respond : How Foolish smth is');
+                        message.channel.send('2) k!foolish = respond : rate the foolishness of smth');
+                        message.channel.send('3) k!fuli = respond with waiting time : Ugh, baboons; Seriously?; Nowhere to run that I cannot run faster; Huwezi');
                         message.channel.send('4) pong = respond : ping');
-                        message.channel.send('5) f!birthdaymac = wish a happy birthday to Mac Guy');
-                        message.channel.send('6) f!beshte, f!kion, f!bunga, f!ono = say a sentence of the charactere and a gif');
+                        message.channel.send('5) k!birthdaymac = wish a happy birthday to Mac Guy');
+                        message.channel.send('6) k!beshte, k!kion, k!bunga, k!ono = say a sentence of the charactere and a gif');
                         
             break;
-        case "f!kion" : await sleep(500 + random(500,0));
+        case "k!kion" : await sleep(500 + random(500,0));
                         message.channel.send('Hevi Kabesa');
                         await sleep(500 + random(500,0));                   
                         message.channel.send('Till the Pridelands end...');
@@ -39,7 +39,7 @@ bot.on('message', async function (message){
                         await sleep(500 + random(500,0));                    
                         message.channel.send('I dont want to end up like Scar.', {file: "https://cdn.discordapp.com/attachments/355274540248596480/356530551143792662/49dc73b2bf47bdacafc40f402b2781df7f4aead9_hq.gif"});
             break;
-        case "f!bunga" : await sleep(500 + random(500,0));
+        case "k!bunga" : await sleep(500 + random(500,0));
                          message.channel.send('Zuka Zama');
                          await sleep(500 + random(500,0));
                          message.channel.send('Unbungalievible');
@@ -48,7 +48,7 @@ bot.on('message', async function (message){
                          await sleep(500 + random(500,0));
                          message.channel.send('Galleallegoes', {file: "https://cdn.discordapp.com/attachments/355274540248596480/356530469266915330/Bunga-hand.gif"});
             break;
-        case "f!ono" :  await sleep(500 + random(500,0));
+        case "k!ono" :  await sleep(500 + random(500,0));
                         message.channel.send('Kinda common knolege really');
                         await sleep(500 + random(500,0));
                         message.channel.send('Hapana!');
@@ -57,7 +57,7 @@ bot.on('message', async function (message){
                         await sleep(500 + random(500,0));
                         message.channel.send('Affirmitive!', {file: "https://cdn.discordapp.com/attachments/355274540248596480/356530809525633025/tumblr_o527fcWzvB1v9ez4qo1_1280.gif"});
             break;
-        case "f!beshte" : await sleep(500 + random(500,0));
+        case "k!beshte" : await sleep(500 + random(500,0));
                           message.channel.send('Twindke Kiboko');
                           await sleep(500 + random(500,0));
                           message.channel.send('Pua');
@@ -67,18 +67,18 @@ bot.on('message', async function (message){
             
             
             
-        case "f!birthdaymac" : message.channel.send('**HAPPY FOOLISH BIRTHDAY Mac Guy !!!!!!!**' , {file: "https://vignette2.wikia.nocookie.net/lionguard/images/5/5b/Fuli-splash.gif"});
+        case "k!birthdaymac" : message.channel.send('**HAPPY FOOLISH BIRTHDAY Mac Guy !!!!!!!**' , {file: "https://vignette2.wikia.nocookie.net/lionguard/images/5/5b/Fuli-splash.gif"});
             break;
             
         }
         
-	if (message.content.toLowerCase().startsWith('f!how are you ')){
+	if (message.content.toLowerCase().startsWith('k!how are you ')){
             
             var reason = message.content.substring(14, message.content.length);
             message.channel.send('How Foolish ' + reason + ' is ?');
             
 	}
-        if (message.content.toLowerCase().startsWith('f!foolish ')){
+        if (message.content.toLowerCase().startsWith('k!foolish ')){
             
             var reason = message.content.substring(10, message.content.length);
             message.channel.send(  reason + ' has ' +  Math.floor(random(100,0)) + '% foolishness'); 
